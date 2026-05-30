3eeebadba7e76d5184d69edea21c899c29c62d33
Irisha Bea Montano <lr.ibmontano@mmdc.mcl.edu.ph>
2026-05-30 22:00:13 +0800

    Re-export SQL dump with create schema

--- payrollsystem_db.sql
+++ payrollsystem_db.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `payrollsystem_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `payrollsystem_db`;
 -- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: payrollsystem_db
@@ -879,4 +881,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-05-30 21:48:48
+-- Dump completed on 2026-05-30 21:59:02
